f38aaeee2eb3c63fe81f37c98dc109d183a513dd
christianbrischetto <christian.brischetto@gmail.com>
2023-10-27 00:52:44 +1100

    cutter

--- TestScripts/test.m
+++ TestScripts/test.m
@@ -2,13 +2,15 @@ clc
 close all
 hold on;
 
-IRB = IRB1200H;
+IRB = UR3;
 home = [0 0 pi/2 0 0 0];
 IRB.model.teach();
 IRB.model.animate(home);
 
 ee = IRB.model.fkine(IRB.model.getpos);
-gripper = scoop;
+ee.t = ee.t - [0,0,-0.01]';
+gripper = cutter;
 gripper.model.base = ee;
 gripper.model.animate(0);
 %PlaceObject('scoop.ply',ee);
+
